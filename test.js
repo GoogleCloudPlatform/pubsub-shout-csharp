c0b2fbbf200d4ebcb19a65ed13f610cd0c1b32c8
Jeffrey Rennie <rennie@google.com>
2015-08-31 11:18:09 -0700

    A test.js that fetches the web page.

---
+++ test.js
@@ -0,0 +1,15 @@
+var system = require('system');
+
+var url = ('https://' + system.env['GOOGLE_APP_VERSION'] + '-dot-'
+    + system.env['GOOGLE_APP_ID'] + '.appspot.com/');
+
+var page = require('webpage').create();
+page.onResourceRequested = function(request) {
+  console.log('Request ' + JSON.stringify(request, undefined, 4));
+};
+page.onResourceReceived = function(response) {
+  console.log('Receive ' + JSON.stringify(response, undefined, 4));
+};
+page.open(url);
+console.log('Done.');
+phantom.exit();
